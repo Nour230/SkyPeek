f1abd8e92a553addea0ab7e0756fce0c60dae0a9
nour <n01555251740@gmail.com>
2025-04-01 21:18:00 +0200

    test DAO

--- app/src/androidTest/java/com/example/skypeek/data/local/WeatherDAOTest.kt
+++ app/src/androidTest/java/com/example/skypeek/data/local/WeatherDAOTest.kt
@@ -1,2 +1,77 @@
 package com.example.skypeek.data.local
 
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.skypeek.data.models.AlarmPojo
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class WeatherDAOTest {
+    private lateinit var dataBase: WeatherDataBase
+    private lateinit var dao: WeatherDao
+
+    @Before
+    fun serup() {
+        dataBase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            WeatherDataBase::class.java
+        ).build()
+        dao = dataBase.dao()
+    }
+
+    @After
+    fun tearDown(){
+        dataBase.close()
+    }
+
+    @Test
+    fun insertAlarm_ShouldInsertAlarmCorectly() = runTest{
+        // Given a test alarm
+        val alarm = AlarmPojo(
+            time = "08:00",
+            date = "2025-04-01"
+        )
+        //when
+        dao.insertAlarm(alarm)
+        //then
+        val alarms = dao.getAllAlarms().first()
+        assertEquals(1, alarms.size)
+        assertEquals("08:00", alarms.first().time)
+        assertEquals("2025-04-01", alarms.first().date)
+    }
+
+    @Test
+    fun getAllAlarms_returnZero_whenNoAlarmsAdded()= runTest {
+        //Given No Alarms in database
+
+        //when getting all alarms
+        val alarms = dao.getAllAlarms().first()
+        //then
+        assert(alarms.isEmpty())
+    }
+
+    @Test
+    fun deleteAlarm_shoulddeleteanalarmsuccessfully() = runTest {
+        // Insert alarm
+        val alarm = AlarmPojo(time = "08:00", date = "2025-04-01", id = 1)
+        dao.insertAlarm(alarm)
+
+        // Delete alarm
+        dao.deleteAlarm(alarm)
+
+        // then check if alarm is deleted
+        val alarms = dao.getAllAlarms().first()
+        assertEquals(0, alarms.size)
+    }
+
+
+}
